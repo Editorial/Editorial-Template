1d2dfee585639e6d31949e1381cf20cb816354cc
Matjaz Korosec <matjaz.korosec@gmail.com>
2011-09-04 23:20:37 +0200

    shtml ver 0.2 (home, cart, help list)

--- shtml/js/script.js
+++ shtml/js/script.js
@@ -1,10 +1,11 @@
 
-if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+var iDevice = (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i)) ? true : false;
+if (iDevice) {
 	var viewportmeta = document.querySelectorAll('meta[name="viewport"]')[0];
 	if (viewportmeta) {
-		viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
+		viewportmeta.content = 'width=device-width,minimum-scale=1.0,maximum-scale=1.0';
 		document.body.addEventListener('gesturestart',function() {
-			viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+			viewportmeta.content = 'width=device-width,minimum-scale=0.25,maximum-scale=1.6';
 		},false);
 	}
 }
@@ -12,7 +13,18 @@ if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) 
 
 $(function(){
 
+	//iOS label fix
+	if (iDevice) {
+		$('label[for]').click(function(){
+			var el = $(this).attr('for');
+			if ($('#' + el + '[type=radio], #' + el + '[type=checkbox]').attr('selected', !$('#' + el).attr('selected'))) return;
+			else $('#' + el)[0].focus();
+		});
+	}
 
+	
+
+	
 //max-width IE6
 /*if ($.browser.msie && $.browser.version < 7) {
 	function maxWidth() {
