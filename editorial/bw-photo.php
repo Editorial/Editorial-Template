2d47ad1780d1b8fc64cfd5673b9669f455fa8e74
Matjaz Korosec <matjaz.korosec@gmail.com>
2011-11-06 13:41:54 +0100

    popravljena pot

--- editorial/bw-photo.php
+++ editorial/bw-photo.php
@@ -13,8 +13,8 @@
  */
 
 //require_once('./../../../wp-load.php');
-require_once('/Users/miha/Projects/Editorial/wordpress/wp-load.php');
-//require_once('/Users/matjazk/Webpages/Personal/editorial/wordpress/wp-load.php');
+//require_once('/Users/miha/Projects/Editorial/wordpress/wp-load.php');
+require_once('/Users/matjazk/Webpages/Personal/editorial/wordpress/wp-load.php');
 
 if (!isset($_GET['photo']) || !isset($_GET['type']) || !Editorial::getOption('black-and-white') || !Editorial::canCache())
 {
