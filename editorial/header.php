15d5b66ae88bf0a2a1e1903b7bc2aaccae96f433
Matjaz Korosec <matjaz.korosec@gmail.com>
2012-01-08 13:14:33 +0100

    TYPEKIT for theme

--- editorial/header.php
+++ editorial/header.php
@@ -69,12 +69,15 @@ also enable the <link> tag below
 <script>try{Typekit.load();}catch(e){}</script>
 */
 ?>
+<!--GOOGLE ANALYTICS
 <script>
-var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-26888938-1']);_gaq.push(['_trackPageview']);
+var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-XXXXXXXX-X']);_gaq.push(['_trackPageview']);
 (function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();
-</script>
+</script>-->
+<script src="http://use.typekit.com/nlh6xyy.js"></script>
+<script>try{Typekit.load();}catch(e){}</script>
 </head>
 
 <body class="<?php echo $EditorialId; ?> <?php echo $EditorialClass; ?>">
