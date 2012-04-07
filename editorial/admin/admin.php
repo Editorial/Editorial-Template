274af5eb0b389167fe9f9db77cda20040dd64c6f
Matjaz Korosec <matjaz.korosec@gmail.com>
2012-04-07 17:30:59 +0200

    admin ico path

--- editorial/admin/admin.php
+++ editorial/admin/admin.php
@@ -98,7 +98,7 @@ class Editorial_Admin
 			'administrator',
 			'editorial',
 			array($this, 'lookAndFeel'),
-			get_bloginfo('template_directory').'favicon.ico'
+			get_bloginfo('template_directory').'/favicon.ico'
 		);
 		add_submenu_page(
 			'editorial',
