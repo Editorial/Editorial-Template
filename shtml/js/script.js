eb30443f1c8fd5ba76ae7ad216a013b431428dd4
Matjaz Korosec <matjaz.korosec@gmail.com>
2012-06-24 22:34:21 +0200

    off-canvas prototip

--- shtml/js/script.js
+++ shtml/js/script.js
@@ -10,18 +10,83 @@
 
  */
 
-var iDevice = (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i)) ? true : false;
-if (iDevice) {
-	var viewportmeta = document.querySelectorAll('meta[name="viewport"]')[0];
-	if (viewportmeta) {
-		viewportmeta.content = 'width=device-width,minimum-scale=1.0,maximum-scale=1.0';
-		document.body.addEventListener('gesturestart',function() {
-			viewportmeta.content = 'width=device-width,minimum-scale=0.25,maximum-scale=1.6';
-		},false);
+// open / close features nav
+var featuresNav = function() {
+	var b = $('body'),
+			fBar = $('#features-bar'),
+			fBarWidth = fBar.width();
+	fBar.stop();
+	if (b.hasClass('active-sidebar')) {
+		fBar.animate({right: '-' + fBarWidth + 'px'}, 100);
+		b.removeClass('active-sidebar');
 	}
-}
+	else {
+		fBar.animate({right:'0'}, 150);
+		b.addClass('active-sidebar');
+	}
+};
 
-$(function(){
+(function() {
+
+	var iDevice = (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i)) ? true : false;
+	if (iDevice) {
+	var viewportmeta = document.querySelectorAll('meta[name="viewport"]')[0];
+		if (viewportmeta) {
+			viewportmeta.content = 'width=device-width,minimum-scale=1.0,maximum-scale=1.0';
+			document.body.addEventListener('gesturestart',function() {
+				viewportmeta.content = 'width=device-width,minimum-scale=0.25,maximum-scale=1.6';
+			},false);
+		}
+	}
+
+	// add/remove body classes based on viewport width
+	var bodyViewportClasses = function() {
+	// testing the display property on the element
+	var b = $('body'),
+			nav_display = $('#features-links').css('display');
+		// testing for display:block (changed in css via media queries)
+		if (nav_display === 'block') {
+			b.removeClass('big-screen').addClass('small-screen');
+		}
+		// testing for display:none (changed in css via media queries)
+		if (nav_display === 'none') {
+			b.removeClass('active-sidebar small-screen').addClass('big-screen');
+		}
+	};
+
+	//off-canvas
+	if ($('body').hasClass('features-new')) {
+
+		bodyViewportClasses();
+
+		$(window).resize(function(){
+			bodyViewportClasses();
+		});
+
+		//features nav
+		$('#show-features').on('click',function(e){
+			e.preventDefault();
+			featuresNav();
+		});
+
+		//geastures
+		function log(event_, obj) {
+		// ignore bubbled handlers
+		//		if ( obj.originalEvent.currentTarget !== obj.originalEvent.target ) { return; }
+			obj.originalEvent.preventDefault();
+			//console.log(obj.description);
+			if (obj.description.lastIndexOf('swipe') != -1 && obj.description.lastIndexOf('left') != -1) featuresNav();
+			if (obj.description == 'tapone') {
+				if(parseInt($('#features-bar').css('right')) == 0) {
+					featuresNav();
+				}
+			}
+		}
+
+		$('.main').on('swipeone',log);
+		$('.main').on('tapone',log);
+
+	}
 
 	//iOS label fix
 	if (iDevice) {
@@ -31,6 +96,9 @@ $(function(){
 			else $('#' + el)[0].focus();
 		});
 	}
+
+	//footer tweets
+	$('.twitter').liveTwitter('editorialtheme',{mode:'user_timeline',limit:1});
 
 	//redirect button
 	$('#checkout').click(function(e){
@@ -94,5 +162,5 @@ $(function(){
 		}
 	});
 
-});
+})();
 
