fced212879f68b034fce48b73bd5ffc2b8f5da5a
Klemen Slavič <krof.drakula@gmail.com>
2012-12-25 14:56:50 +0100

    Initialisation step complete

--- touchgallery/lib/gallery.js
+++ touchgallery/lib/gallery.js
@@ -1,5 +1,145 @@
 (function($) {
 
+    /**
+     * TouchGallery constructor
+     * @param Object options A hash map of options passed to initialise the component
+     */
+    function TouchGallery(options) {
+        this.container = $(options.container);
+        this.items     = options.items;
+
+        // set the initial values
+        this.position    = null;
+        this.currentItem = null;
+
+        // bind event handlers' context to this component instance
+        this.constructor.boundHandlers.forEach(function(name) {
+            this[name] = bind(this, this[name]);
+        }, this);
+
+        // hook up events
+        $(window).resize(this.handleResize);
+
+        // start preloading images before initialising structure
+        // to allow measuring images sizes before initial display
+        this.preloadImages(this.init);
+    }
+
+    TouchGallery.boundHandlers = [
+        'handleTouchStart', 'handleTouchMove', 'handleTouchEnd', 'handleTouchCancel',
+        'handleMouseDown', 'handleMouseMove', 'handleMouseUp',
+        'handleTap', 'handleClick',
+        'handleResize',
+        'init'
+    ];
+
+    TouchGallery.prototype.template =
+        '<div class="touch-gallery">' +
+            '<ul class="items"></ul>' +
+            '<div class="top-bar"></div>' +
+            '<div class="bottom-bar"></div>' +
+        '</div>';
+
+    TouchGallery.prototype.preloadImages = function(callback) {
+        var waitingToLoad = this.items.length;
+
+        this.items.forEach(function(item) {
+            item.img = new Image;
+            item.img.onload = done;
+            item.img.src = item.src;
+        });
+
+        function done() { if (!--waitingToLoad) callback(); }
+    };
+
+    /**
+     * Initialises the component structure and positions the images
+     */
+    TouchGallery.prototype.init = function() {
+        this.container.append(this.template);
+        var list = this.container.find('.items');
+        var listRatio = list.width() / list.height();
+        var containerWidth = this.container.width();
+        this.items.forEach(function(item) {
+            var el = $('<li class="' + item.type + '"><img src="' + item.src + '"/></li>').appendTo(list);
+        }, this);
+        this.repositionImages();
+        this.moveTo(0, true);
+    };
+
+    TouchGallery.prototype.repositionImages = function() {
+        var list = this.container.find('.items')
+        list.children().each(function() {
+            var img = $(this).find('img');
+            // if the image has a higher aspect ratio than the list container
+            // we have to align it vertically
+            if (img.height() < list.height())
+                img.css('margin-top', (list.height() - img.height()) / 2 + 'px');
+        });
+    };
+
+    /**
+     * (Internal) Snaps to the nearest image
+     */
+    TouchGallery.prototype.snap = function() {
+
+    };
+
+    /**
+     * Moves the gallery to the specified item (zero-indexed)
+     * @param  {Number} idx [description]
+     */
+    TouchGallery.prototype.moveTo = function(idx, withoutTransition) {
+
+    };
+
+    /**
+     * Advances to the next item. When there are no more items, the function
+     * silently ignores the command.
+     */
+    TouchGallery.prototype.goToNext = function() {
+
+    };
+
+    /**
+     * Moves the gallery back one item. If already at the first item, the
+     * function silently ignores the command.
+     */
+    TouchGallery.prototype.goToPrevious = function() {
+        if (this.currentItem > 0) this.moveTo(this.currentItem - 1);
+    };
+
+
+    // interaction handlers
+    TouchGallery.prototype.handleTouchStart = function(ev) {};
+    TouchGallery.prototype.handleTouchMove = function(ev) {};
+    TouchGallery.prototype.handleTouchEnd = function(ev) {};
+    TouchGallery.prototype.handleTouchCancel = function(ev) {};
+
+    TouchGallery.prototype.handleMouseDown = function(ev) {};
+    TouchGallery.prototype.handleMouseMove = function(ev) {};
+    TouchGallery.prototype.handleMouseUp = function(ev) {};
+
+    TouchGallery.prototype.handleResize = function() {
+        this.repositionImages();
+    };
+
+
+    // export the class to the global namespace
+    this.TouchGallery = TouchGallery;
+
+
     
+    /**
+     * Binds a function to the supplied context
+     * @param  {Object}   ctx The context object
+     * @param  {Function} fn  The function to bind
+     * @return {Function}     The bound function
+     */
+    function bind(ctx, fn) {
+        return function() {
+            return fn.apply(ctx, arguments);
+        };
+    }
 
 })(jQuery);
