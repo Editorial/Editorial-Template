cdf8ca0a797e20bfebeaa5ebb92309aa0022282d
makeup <matjaz.korosec@gmail.com>
2013-01-06 21:50:19 +0100

    merge

--- marketing/assets/js/script.js
+++ marketing/assets/js/script.js
@@ -10,18 +10,40 @@
 
  */
 
-var iDevice = (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i)) ? true : false;
-if (iDevice) {
-	var viewportmeta = document.querySelectorAll('meta[name="viewport"]')[0];
-	if (viewportmeta) {
-		viewportmeta.content = 'width=device-width,minimum-scale=1.0,maximum-scale=1.0';
-		document.body.addEventListener('gesturestart',function() {
-			viewportmeta.content = 'width=device-width,minimum-scale=0.25,maximum-scale=1.6';
-		},false);
-	}
-}
+(function() {
 
-$(function(){
+	var iDevice = (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i)) ? true : false;
+	if (iDevice) {
+	var viewportmeta = document.querySelectorAll('meta[name="viewport"]')[0];
+		if (viewportmeta) {
+			viewportmeta.content = 'width=device-width,minimum-scale=1.0,maximum-scale=1.0';
+			document.body.addEventListener('gesturestart',function() {
+				viewportmeta.content = 'width=device-width,minimum-scale=0.25,maximum-scale=1.6';
+			},false);
+		}
+	}
+
+	//Off canvas
+	var page = $('body');
+
+	$('#show-features').on('click', function(e) {
+		e.preventDefault();
+		changeActive();
+	});
+
+	var changeActive = function() {
+		if (page.hasClass('not-active')) {
+			page.removeClass('not-active').addClass('active-sidebar');
+		} else if (page.hasClass('active-sidebar')) {
+			page.removeClass('active-sidebar').addClass('not-active');
+		}
+	};
+
+	page.removeClass('active-sidebar').addClass('not-active');
+
+	$(window).on('resize', function() {
+		page.removeClass('active-sidebar').addClass('not-active');
+	});
 
 	//iOS label fix
 	if (iDevice) {
@@ -96,6 +118,94 @@ $(function(){
 			$('#total').val(currency+domains*parseFloat($('#price-c').val().substr(1)));
 		}
 	});
+
+
+
+
+
+	//price flow
+	if($('ul.price-flow').length) {
+
+		function flow(soldLicences) {
+			var mobile = $('ul.pf-mobile'),
+					graph = (mobile.css('display') == 'block') ? mobile : $('ul.pf-other'),
+					countActiveSteps = 0,
+					pass = 0,
+					limits = [];
+
+			graph.find('li').removeClass('current').removeClass('sold').each(function(){
+				var t =  $(this).find('span').text();
+				if(t != '') limits.push(t);
+			});
+
+			for(var i = 0; i + 1 < limits.length + 1; i++) {
+				var nthI = graph.find('li:nth-child(' + i + ')'),
+						nthPlus1 = graph.find('li:nth-child(' + parseInt(i + 1, 10) + ')');
+				//active & current
+				if (parseInt(limits[i], 10) <= soldLicences) {
+					nthI.addClass('active');
+					countActiveSteps++;
+					if (parseInt(limits[i+1], 10) > soldLicences) {
+						nthPlus1.addClass('current');
+					}
+				} else {
+					nthI.removeClass('active');
+				}
+			}
+
+			if (countActiveSteps > 2) {
+				for(var j = countActiveSteps; j > 0 ; j--) {
+					var nthJ = graph.find('li:nth-child(' + j + ')'),
+							isStep = nthJ.attr('class').match(/step-/i) ? true : false;
+					if(pass === 1) {
+						nthJ.removeClass('active').addClass('sold');
+					}
+					if (isStep) {
+						pass = 1;
+
+						//update price tag's price
+						//console.log(nthJ.find('em').text().substr(1));
+						//$('#price-tag').html(nthJ.find('em').text().substr(1));
+
+					}
+				}
+			}
+		}
+
+		//testingTOOL
+		(function() {
+			$('section.licencing').append('' +
+			'<div id="counter" style="position:absolute;left:50%;bottom:8px;z-index:999;background:yellow;">' +
+			'	<div style="min-width:30px;min-height:30px;">' +
+			'		<div style="position:relative;display:none;" id="counter-show">' +
+			'			<label for="sold">Sold: </label>' +
+			'			<input type="text" id="sold" value="0" style="border:1px solid lime;padding:6px 0 4px;width:50px;font-size:20px;text-align:center" maxlength="4">' +
+			'			<div>' +
+			'				<a href="#" id="go-up" style="font-weight:bold;font-size:15px;color:red;padding:5px;clear:left;" title="+1">+1</a>' +
+			'				<a href="#" id="go-down" style="font-weight:bold;font-size:15px;color:red;padding:5px;" title="-1">-1</a>' +
+			'				<a href="#" id="go-50up" style="font-weight:bold;font-size:15px;color:red;padding:5px;" title="+50">+50</a>' +
+			'				<a href="#" id="go-50down" style="font-weight:bold;font-size:15px;color:red;padding:5px;" title="-50">-50</a>' +
+			'				<a href="#" id="go-25up" style="font-weight:bold;font-size:15px;color:red;padding:5px;" title="+20">+20</a>' +
+			'				<a href="#" id="go-25down" style="font-weight:bold;font-size:15px;color:red;padding:5px;" title="-20">-20</a>' +
+			'			</div>' +
+			'		</div>' +
+			'		<a href="#" id="on-off" style="font-weight:bold;font-size:15px;color:lime;position:absolute;top:5px;right:5px;" title="ON / OFF">[<span id="p">+</span>]</a>' +
+			'	</div>' +
+			'</div>');
+			var licences = $('#sold');
+			$('#go-up, #go-down, #go-50up, #go-50down, #go-25up, #go-25down').on('click', function(e){e.preventDefault();
+				var lVal = parseInt(licences.val(), 10), tText = parseInt($(this).text(), 10), lANDt = lVal + tText;
+				if(lANDt >= 0) { licences.val(lANDt); licences.trigger('change'); }
+			});
+			$('#on-off').on('click', function(e) {e.preventDefault();$('#counter-show').toggle().css('display') == 'block' ? $('#p').html('-') : $('#p').html('+');});
+			licences.on('change', function(){ flow(licences.val()); });
+		})();
+
+		//onResize
+		$(window).on('resize', function(){ flow($('#sold').val()); });
+
+	}
+
 	
 	// purchase discount check
 	$('#promo').change(function(e){
@@ -118,5 +228,5 @@ $(function(){
 		return;
 	});
 
-});
+})();
 
