99ccde7ba0b428b7472305dd4e89ec28491eee67
Miha Hribar <miha@hribar.info>
2013-01-13 01:16:55 +0100

    bw and retina images are now generated on the spot and cached in the /uploads folder

--- editorial/loop-posts.php
+++ editorial/loop-posts.php
@@ -10,57 +10,53 @@
 
 if (have_posts())
 {
-	global $EditorialId;
-	$exposed = false;
-	$i = 1;
-	// enter the Loop
-	echo '	<section id="exposed">
+    global $EditorialId;
+    $exposed = false;
+    $i = 1;
+    // enter the Loop
+    echo '	<section id="exposed">
 ';
-	while ( have_posts() )
-	{
-		the_post();
-		// skip strange posts
-		if (!$post) continue;
-		$thumbId = get_post_thumbnail_id();
-		if (!$exposed)
-		{
-			// show exposed
-			$exposed = true;
+    while ( have_posts() )
+    {
+        the_post();
+        // skip strange posts
+        if (!$post) continue;
+        $thumbId = get_post_thumbnail_id();
+        if (!$exposed)
+        {
+            // show exposed
+            $exposed = true;
 ?>
-		<article class="hentry">
-			<div class="detail">
+        <article class="hentry">
+            <div class="detail">
 <?php
-				Editorial::postFooter();
+                Editorial::postFooter();
 ?>
 <?php
-				Editorial::postHeader();
+                Editorial::postHeader();
 ?>
-				<?php Editorial::postExcerpt();?>
-			</div>
+                <?php Editorial::postExcerpt();?>
+            </div>
 <?php
-			//Editorial::postFigure($thumbId, $EditorialId == 'home' ? 'landscape' : 'portrait', true);
-			//$thumbnailUrl = Editorial::getResponsiveImageUrl ( $thumbId, 'full' );
+            Editorial::postFigure($thumbId, $EditorialId == 'home' ? 'landscape' : 'portrait', true);
 ?>
-			<figure>
-				<a href="<?php the_permalink(); ?>" rel="bookmark"><img src="<?php echo Editorial::getResponsiveImageUrl ( $thumbId, 'full', (bool)Editorial::getOption('black-and-white') ) ?>" alt="<?php the_title(); ?>"></a>
-			</figure>
-		</article>
-	</section>
-	<section class="featured">
+        </article>
+    </section>
+    <section class="featured">
 <?php
-		}
-		else
-		{
-			// show featured
-			include('featured-article.php');
-			$i++;
-		}
-	}
-	echo '
-	</section>';
+        }
+        else
+        {
+            // show featured
+            include('featured-article.php');
+            $i++;
+        }
+    }
+    echo '
+    </section>';
 }
 else
 {
-	// no posts -> error page?
-	dump('no posts');
+    // no posts -> error page?
+    dump('no posts');
 }
