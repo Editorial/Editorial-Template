758a5ae75607611e2cb08a88838caaabffc0f31f
makeup <matjaz.korosec@gmail.com>
2013-02-12 23:50:14 +0100

    home page form

--- marketing/assets/js/script.js
+++ marketing/assets/js/script.js
@@ -228,12 +228,40 @@
 		return;
 	});
 
+	//regex email validation
+	function isValidEmail(email) {
+		var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+		return pattern.test(email);
+	}
+
 	//inquiry form
-	$('form.wpcf7-form').find('h2').click(function(){
-		var t = $(this);
-		t.toggleClass('opened');
-		t.next('div.adapt').slideToggle();
-	});
+	var wpcf7 = $('form.wpcf7-form'),
+			fields = $('#msg,#name,#uemail');
+
+	if (wpcf7.length) {
+
+		//toggle form
+		wpcf7.find('h2').click(function(){
+			var t = $(this);
+			t.toggleClass('opened');
+			t.next('div.adapt').slideToggle();
+		});
+
+		//green button
+		fields.on('keydown keyup change', function(){
+			var wpcf7s = wpcf7.find('input.wpcf7-submit'),
+					invalid = 0;
+
+			fields.each(function(){
+				var t = $(this);
+				if (t.hasClass('watermark') || t.val() == '') invalid++;
+			});
+
+			wpcf7s.removeClass('go-green');
+			if (invalid == 0 && isValidEmail($('#uemail').val())) wpcf7s.addClass('go-green');
+
+		});
+	}
 
 })();
 
