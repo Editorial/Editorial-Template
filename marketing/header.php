e7f8def1fc5c677f01fbb1d8bdc6f9a3121561f4
makeup <matjaz.korosec@gmail.com>
2013-03-26 07:09:33 +0100

    newsletter 03

--- marketing/header.php
+++ marketing/header.php
@@ -77,12 +77,15 @@
 <?php
 	}
 ?>
-<script>
-var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-23356248-1']);_gaq.push(['_trackPageview']);
+<script>var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-23356248-1']);_gaq.push(['_trackPageview']);
 (function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();
-</script>
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script>
+<script>var _kmq = _kmq || [];var _kmk = _kmk || 'bbf0f86fa5f0a4724198c2107f74580a4b7cd4e4';
+function _kms(u){setTimeout(function(){var d = document, f = d.getElementsByTagName('script')[0],
+s = d.createElement('script');s.type = 'text/javascript'; s.async = true; s.src = u;
+f.parentNode.insertBefore(s, f);}, 1);}_kms('//i.kissmetrics.com/i.js');
+_kms('//doug1izaerwt3.cloudfront.net/' + _kmk + '.1.js');</script>
 </head>
 
 <body <?php body_class(); ?>>
