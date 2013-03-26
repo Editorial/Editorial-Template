b310921c30fb7ec644bcaf3034cc9ec3203cb61a
makeup <matjaz.korosec@gmail.com>
2013-03-26 08:09:12 +0100

    GA koda

--- marketing/header.php
+++ marketing/header.php
@@ -81,11 +81,11 @@
 (function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script>
-<script>var _kmq = _kmq || [];var _kmk = _kmk || 'bbf0f86fa5f0a4724198c2107f74580a4b7cd4e4';
-function _kms(u){setTimeout(function(){var d = document, f = d.getElementsByTagName('script')[0],
-s = d.createElement('script');s.type = 'text/javascript'; s.async = true; s.src = u;
-f.parentNode.insertBefore(s, f);}, 1);}_kms('//i.kissmetrics.com/i.js');
-_kms('//doug1izaerwt3.cloudfront.net/' + _kmk + '.1.js');</script>
+<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-39541867-1', 'editorialtemplate.com');ga('send', 'pageview');</script>
 </head>
 
 <body <?php body_class(); ?>>
